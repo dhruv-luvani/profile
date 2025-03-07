285646b09b97aaf095cb7f2dad425b1101ffd9b2
dhruvsemicolon <dhruv.semicolon@gmail.com>
2025-03-07 15:50:21 +0530

    feat: BlurText for animated text effects, update HomeHeroSection for enhanced animations and styling

--- src/components/pages/Home/HomeHeroSection.jsx
+++ src/components/pages/Home/HomeHeroSection.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ClickSpark from '../../widgets/ClickSpark'
 import { ReactTyped } from "react-typed";
 import bgProfileImg from '../../../assets/images/profile_42.png'
+import BlurText from '../../widgets/BlurText';
 
 export default function HomeHeroSection() {
+  const [animationComplete, setAnimationComplete] = useState(false)
   return (
     <div className="bg-gradient-to-br from-green-900 via-transparent to-cyan-700 bg-200 animate-bg-scroll text-white ">
       <ClickSpark>
@@ -13,20 +15,29 @@ export default function HomeHeroSection() {
             backgroundImage: `url(${bgProfileImg})`,
             backgroundSize: "auto 85%"
           }}>
-          <h1 className="text-5xl font-bold drop-shadow-black">Hello, I'm Dhruv Luvani</h1>
-          <ReactTyped
-            strings={[
-              "React Js Developer",
-              "Next Js Developer",
-            ]}
-            typeSpeed={100}
-            backSpeed={30}
-            backDelay={1000}
-            attr="placeholder"
-            loop
-          >
-            <input className='text-2xl italic bg-transparent outline-none placeholder:text-white text-slate-300 mt-2 cursor-default' type="text" disabled />
-          </ReactTyped>
+          <BlurText
+            text="Hello, I'm Dhruv Luvani!"
+            delay={100}
+            animateBy="letters"
+            direction="top"
+            className="text-5xl font-bold drop-shadow-black"
+            onAnimationComplete={() => setAnimationComplete(true)}
+          />
+          {animationComplete ? (
+            <ReactTyped
+              strings={[
+                "React Js Developer",
+                "Next Js Developer",
+              ]}
+              typeSpeed={100}
+              backSpeed={30}
+              backDelay={1000}
+              attr="placeholder"
+              loop
+            >
+              <input className='text-2xl italic bg-transparent outline-none placeholder:text-white text-slate-300 mt-2 cursor-default' type="text" disabled />
+            </ReactTyped>
+          ) : (<div className='h-10' />)}
         </div>
       </ClickSpark>
     </div>
