9c3b7c7c72569634500c4fc339c1173dd48e2f3f
Dhruv Luvani <dvluvani@gmail.com>
2025-04-08 10:45:49 +0530

    feat: animated cursor

--- src/index.js
+++ src/index.js
@@ -9,12 +9,27 @@ import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ScrollContainer } from 'react-nice-scroll';
+import AnimatedCursor from 'react-animated-cursor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <AnimatedCursor
+          innerSize={5}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={false}
+          innerStyle={{
+            backgroundColor: 'cyan'
+          }}
+          outerStyle={{
+            border: '3px solid lightgray'
+          }}
+        />
         <ScrollContainer activeSmoothScrollOnTouchDevice={false} alwaysShowTracks={true} >
           <App />
         </ScrollContainer>
